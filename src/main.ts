4e4bc39c0dcd617d3c7e8bc8ad4b28bb77b845a3
Belgacem BEN AMIRA <Belgacem.BENAMIRA@esprim.tn>
2024-05-06 14:54:32 +0100

    refactor : add swagger to use in test

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,27 @@
-/**
-    * @description      : 
-    * @author           : belgacem
-    * @group            : 
-    * @created          : 18/02/2024 - 23:20:07
-    * 
-    * MODIFICATION LOG
-    * - Version         : 1.0.0
-    * - Date            : 18/02/2024
-    * - Author          : belgacem
-    * - Modification    : 
-**/
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuration de Swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('SKO API Documentation')
+    .setDescription('Documentation pour SKO API')
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/api', app, swaggerDocument);
+
+  // Servir les fichiers statiques depuis le dossier "uploads"
+  app.use('../ScoProject/uploads', express.static('uploads'));
+
+  // Écouter sur le port 5000
   await app.listen(5000);
 }
+
 bootstrap();
